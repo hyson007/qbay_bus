f47e1c2acaaff80a87e5f0571cb211d454f13688
jack yao <hyson007@gmail.com>
2022-01-19 15:02:17 +0800

    update

---
+++ settings.py
@@ -0,0 +1,8 @@
+headers = {
+    'uniqueuserid': "d50515fd-c1d0-4645-bee1-d82e2649e262",
+    'accountkey': "GYjX0Mpb0lsJkJP0sh9s1w==",
+    'cache-control': "no-cache",
+    'postman-token': "e38c3fd7-3817-1a06-65fd-b8aaada28df0"
+    }
+
+url = "http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2"
